901289c0a4a2eb6e52afad5d8cbc93de8564f2c2
Alejandro Gonzalez <sieg_1594@hotmail.com>
2024-09-12 15:06:19 -0400

    form

--- src/app/shared/form-personal/form-personal.component.ts
+++ src/app/shared/form-personal/form-personal.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CardComponent } from "../card/card.component";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-personal',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CardComponent, ReactiveFormsModule],
   templateUrl: './form-personal.component.html',
   styleUrl: './form-personal.component.scss'
 })
 export class FormPersonalComponent {
+  private readonly _formBuilder = inject(FormBuilder)
+  aboutYouForm = this._formBuilder.group({
+    nombre: ['', Validators.required],
+    apellidos: ['', Validators.required],
+    email: ['', Validators.required, Validators.email],
+    profesion: ['', Validators.required],
+    sobreTi: ['', Validators.required],
+    descripciónGeneral: ['', Validators.required],
+  })
 
+  nextForm(){
+    console.log(this.aboutYouForm.value);
+  }
 }
